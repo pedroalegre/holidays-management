9206bd9b7d534a4d2c61dfd29654ca66b821970d
Pedro Alegre <alegre.p@gmail.com>
2019-05-21 14:00:03 +0200

    Added security

--- src/main/java/com/project/holidays/controllers/WebController.java
+++ src/main/java/com/project/holidays/controllers/WebController.java
@@ -1,13 +1,24 @@
 package com.project.holidays.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MyController {
+public class WebController {
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello!";
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
     }
 }
